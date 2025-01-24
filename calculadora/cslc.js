e4679093fb086a677a2a753691aaf3d5aad3620c
victor06-git <asensiovector@gmail.com>
2025-01-24 11:49:53 +0100

    .

--- calculadora/cslc.js
+++ calculadora/cslc.js
@@ -11,7 +11,7 @@ buttons.forEach(button => {
 
         if (buttonText === '=') {
             // If the equal button is pressed
-            if (currentInput !== '' && operator !== null) {
+            if (currentInput !== '' && operator !== null) { //If there is a number and an operator
                 // Make the calculation
                 previousResult = calculate(previousResult, parseFloat(currentInput), operator);
                 resultadoLabel.textContent = previousResult; // Show the result
@@ -20,16 +20,25 @@ buttons.forEach(button => {
             }
         } else if (['+', '-', '×', '÷'].includes(buttonText)) {
             // Si se presiona un operador
-            if (currentInput !== '') {
-                if (previousResult === null) {
-                    previousResult = parseFloat(currentInput); // Guarda el primer número
+            if (currentInput !== '') { // If there is a number
+                if (previousResult === null) { // If there is no previous result
+                    previousResult = parseFloat(currentInput); // Save the number as previous result
                 } else {
-                    previousResult = calculate(previousResult, parseFloat(currentInput), operator); // Calcula el resultado
+                    previousResult = calculate(previousResult, parseFloat(currentInput), operator); // Make the calculation with the previous result
+                    resultadoLabel.textContent = previousResult;
                 }
-                operator = buttonText; // Guarda el operador
-                currentInput = ''; // Reinicia el input actual
-                resultadoLabel.textContent = ''; //Restart the label
+                operator = buttonText; // Save the operator
+                currentInput = ''; // Restart the actual input
+            } else if (previousResult !== null) {
+                operator = buttonText; // Save the operator
             }
+        } else if (buttonText === 'C') {
+            // If the clear button is pressed
+            currentInput = ''; // Clear the current input
+            previousResult = null; // Clear the previous result
+            operator = null; // Clear the operator
+            resultadoLabel.textContent = '0'; // Reset the display to 0
+        
         } else {
             // If it is a number button pressed 
             currentInput += buttonText; // Add the nummber to the current input
